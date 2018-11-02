152ab684ff04ad177e16ed5c0852bc9dc09cf2d1
Ross Maniaci <roscoman1986@gmail.com>
2018-11-02 12:27:39 -0700

    Initial Commit

---
+++ RMFlickr/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  RMFlickr
+//
+//  Created by Ross Maniaci on 11/2/18.
+//  Copyright © 2018 Ross Maniaci. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
