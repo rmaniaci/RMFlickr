eb0697ffecdf253eb82d064f2e4109cc75f38d7d
Ross Maniaci <roscoman1986@gmail.com>
2018-11-05 11:50:36 -0700

    Build 8.
    Set up a basic UI test.

--- RMFlickrUITests/RMFlickrUITests.swift
+++ RMFlickrUITests/RMFlickrUITests.swift
@@ -9,26 +9,40 @@
 import XCTest
 
 class RMFlickrUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app = XCUIApplication()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
-    func testExample() {
+    func testShowThumbnailTableView() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        
+        // given
+        let rmFlickr = app.navigationBars["RMFlickr"].otherElements["RMFlickr"]
+        
+        // then
+        app.navigationBars["RMFlickr"].otherElements["RMFlickr"].tap()
+        
+        // when
+        XCTAssert(rmFlickr.exists)
     }
-
 }
